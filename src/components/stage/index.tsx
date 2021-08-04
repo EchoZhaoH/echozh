e2ca17e0247abfd1af2481015b81e6593da4a292
echozh <572712675@qq.com>
2021-08-04 20:09:58 +0800

    feat: update stage

--- src/components/stage/index.tsx
+++ src/components/stage/index.tsx
@@ -9,9 +9,24 @@ import "./style.scss"
 export function Stage() {
   return (
     <div className="stage">
-      <StageItem bg={stage1} />
-      <StageItem bg={stage2} />
-      <StageItem bg={stage3} />
+      <StageItem 
+        bg={stage1} 
+        topBgColor="rgba(195, 214, 223, 0.5)"
+        bottomBgColor="rgba(92, 121, 94, 0.5)"
+        topText="我是谁"
+      />
+      <StageItem
+        bg={stage2}
+        topBgColor="rgba(186, 228, 249, 0.5)"
+        bottomBgColor="rgba(126, 107, 66, 0.5)"
+        topText="我的记录"
+      />
+      <StageItem
+        bg={stage3}
+        topBgColor="rgba(231, 217, 201, 0.5)"
+        bottomBgColor="rgba(105, 148, 177, 0.5)"
+        topText="我的瞎想"
+      />
     </div>
   )
 }
