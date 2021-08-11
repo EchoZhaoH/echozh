95be88661c66629dbefd51a859ea7a9bccb4f003
echozh <572712675@qq.com>
2021-08-11 20:52:28 +0800

    feat: update three

--- src/components/v-world/index.tsx
+++ src/components/v-world/index.tsx
@@ -1,26 +1,36 @@
 import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 
+import bgImg from '@assets/images/stage-1.jpeg'
+
 export function VWorld() {
   const ref = useRef<HTMLDivElement>(null)
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-  const renderer = new THREE.WebGLRenderer();
-
+  
   // 创建场景
   useEffect(() => {
     const el = ref.current
     if (!el) {
       return
     }
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  
+    const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     el.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const geometry = new THREE.SphereGeometry();
+    const material = new THREE.MeshBasicMaterial({
+      map: new THREE.TextureLoader().load(bgImg)
+    });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
+    const bgTexture = new THREE.TextureLoader().load(bgImg)
+    bgTexture.mapping = THREE.CubeRefractionMapping
+    bgTexture.wrapS = THREE.RepeatWrapping
+    bgTexture.wrapT = THREE.RepeatWrapping
+    bgTexture.repeat.set(1, 1)
+    scene.background = bgTexture
 
     camera.position.z = 5;
     let timer: number | null = null
