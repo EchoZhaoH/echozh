0a8563b6cc54d9b0c6200b5bf206030188b8be09
echozh <572712675@qq.com>
2021-08-20 21:09:53 +0800

    feat: update layout

--- src/components/entry/index.tsx
+++ src/components/entry/index.tsx
@@ -2,8 +2,11 @@ import { request } from '@/api';
 import { getHref } from '@/utils/getHref';
 import { AnchorButton, Navbar } from '@blueprintjs/core'
 import { Article } from '@components/article'
+import { Film } from '@components/film'
 import { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+
+import './style.scss'
 
 interface Article { id: string; title: string; date: string }
 
@@ -18,37 +21,8 @@ export function Entry() {
     })
   }, [])
   return (
-    <div className="container-bg">
-      <Navbar>
-        <Navbar.Group>
-          <Navbar.Heading>
-            <AnchorButton text="ECHOZH" href={rootHref} outlined={false} minimal />
-          </Navbar.Heading>
-          <Navbar.Divider />
-          <AnchorButton
-            icon="document"
-            text="文章"
-            outlined={false}
-            minimal
-            href={rootHref}
-          />
-          <AnchorButton
-            icon="hand-right"
-            text="瞎搞"
-            outlined={false}
-            minimal
-            href={vWorldHref}
-          />
-        </Navbar.Group>
-      </Navbar>
-      <div>
-        {list.map(l => (
-          <div key={l.id}>
-            <h3><Link to={`/articles/${l.id}`}>{l.title}</Link></h3>
-            <p>{l.date}</p>
-          </div>
-        ))}
-      </div>
+    <div className="content">
+      <Film />
     </div>
   );
 }
