807e5b9de45a25dfdee3e4a6657ad264a28ee7bc
echozh <572712675@qq.com>
2021-08-30 20:46:22 +0800

    refactor class dir

--- src/route.tsx
+++ src/route.tsx
@@ -7,7 +7,7 @@ import {
 import App from './App'
 import { Article } from './components/article'
 import VWorld from './components/v-world'
-import { Sierpinski } from './components/webgl/sierpinski'
+import { Sierpinski } from './components/webgl/class1/sierpinski'
 import { useDark } from './hooks'
 
 export function RoutesMap() {
