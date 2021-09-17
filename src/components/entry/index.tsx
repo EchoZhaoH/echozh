3e34a8f24e2470bb9ba792e7dad19b1b69c28119
echozh <572712675@qq.com>
2021-09-17 20:12:26 +0800

    feat: upgrade dem path

--- src/components/entry/index.tsx
+++ src/components/entry/index.tsx
@@ -3,18 +3,15 @@ import githubLog from '@assets/images/github-log.png';
 
 import './style.scss'
 import { Card, H4, H5 } from '@blueprintjs/core';
-
-const DAY = 24*60*60*1000
+import { WEBGLClasses } from '@/data/webgl-class';
 
 export function Entry() {
-  const days = Math.floor((Date.now() - new Date('2021-08-06').getTime()) / DAY)
   return (
     <div className="content">
       <Card interactive>
         <H4>Hi 我是 Echo</H4>
-        <p>一名前端工程师，目前就职于 ZOOM，喜欢折腾。这是我的
+        <p>一名前端但是不擅长前端的前端工程师，目前就职于 ZOOM。
           <Link to="https://github.com/EchoZhaoH"><img src={githubLog} className="github-log" />github</Link>，欢迎关注我。<br />
-          目前在减肥，坚持了 <strong>{days}</strong> 天。同时也在培养自己的阅读习惯（刷 b 站停不下来）。
         </p>
       </Card>
       <Card interactive>
@@ -31,21 +28,11 @@ export function Entry() {
               <H5>交互式计算机图形学</H5>
               <p>基于 WebGL 的自顶向下方法（第七版）<br />这有我跟着练习的 demo</p>
               <ul>
-                <li>
-                  <Link to="/v-world">demo</Link>
-                </li>
-                <li>
-                  <Link to="/webgl/class1/demo1">demo1</Link>
-                </li>
-                <li>
-                  <Link to="/webgl/class1/demo2">demo2</Link>
-                </li>
-                <li>
-                  <Link to="/webgl/class1/demo3">demo3</Link>
-                </li>
-                <li>
-                  <Link to="/webgl/class1/demo3">demo4</Link>
-                </li>
+                {WEBGLClasses.map(cls => (
+                  <li key={cls.path}>
+                    <Link to={cls.path}>{cls.title}</Link>
+                  </li>
+                ))}
               </ul>
             </Card>
           </li>
---
+++ src/data/webgl-class.ts
@@ -0,0 +1,26 @@
+interface WEBGLClass {
+  title: string;
+  path: string;
+}
+export const WEBGLClasses: WEBGLClass[] = [
+  {
+    title: 'demo',
+    path: '/v-world'
+  },
+  {
+    title: 'demo1',
+    path: '/webgl/class1/demo1'
+  },
+  {
+    title: 'demo2',
+    path: '/webgl/class1/demo2'
+  },
+  {
+    title: 'demo3',
+    path: '/webgl/class1/demo3'
+  },
+  {
+    title: 'demo4',
+    path: '/webgl/class1/demo4'
+  },
+]
