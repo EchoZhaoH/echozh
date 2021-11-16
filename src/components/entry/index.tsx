01dcf4ca5a82463e6b59615a342d8147adc0ea8f
echozh <572712675@qq.com>
2021-11-16 20:25:08 +0800

    update

--- src/components/entry/index.tsx
+++ src/components/entry/index.tsx
@@ -1,9 +1,8 @@
-import { Link } from 'react-router-dom';
-
 import './style.scss'
 import TypeIt from 'typeit-react';
 import { Header } from '@components/header';
 import { Pet } from '@/components/pet'
+import { activeDepot } from '@/data';
 
 export function Entry() {
   return (
@@ -23,6 +22,16 @@ export function Entry() {
             </p>
           </div>
         </TypeIt>
+      <div className="mt-4">
+          <h4 className="text-center mt-4 font-700">目前正在更新的仓库</h4>
+          <ul className="text-center">
+            {activeDepot.map(d => (
+              <li key={d.url}>
+                <a href={d.url}>{d.title}</a>
+              </li>
+            ))}
+          </ul>
+      </div>
       </div>
     </>
   );
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,1 +1,12 @@
 export const githubSrc = 'https://github.com/EchoZhaoH';
+
+export const activeDepot = [
+  {
+    url: 'https://github.com/EchoZhaoH/vscode-lsp-demo',
+    title: 'Vscode Lsp Demo (一个中文格式化插件)'
+  },
+  {
+    url: 'https://github.com/EchoZhaoH/learn-rust',
+    title: 'Rust 学习'
+  }
+]
